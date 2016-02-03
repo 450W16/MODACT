f6e1db6f6142fbc539dce777d8f03562c8bd16ca
Aaron Tse <akt@ualberta.ca>
2016-02-02 18:46:49 -0700

    Added ladder and bridge transformations. Still buggy, documented in their respective files

--- src/abilities/switch.py
+++ src/abilities/switch.py
@@ -1,12 +1,17 @@
 import pygame
 from ability import Ability
 
+# Switch characters
 class Switch(Ability):
 
 	def __init__(self):
 		Ability.__init__(self, "switch", pygame.K_f)
 
 	def cast(self, c):
+		# Revert if switching to biggie
+		if pygame.K_r in c.AI.abilities and c.AI.locked == True:
+			c.AI.abilities[pygame.K_r].cast(c)
+
 		# set change to 0 otherwise you get weird behaviour when
 		# moving and switching
 		c.player.delta_x, c.AI.delta_x = c.AI.delta_x, c.player.delta_x
